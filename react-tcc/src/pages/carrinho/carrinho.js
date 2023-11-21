9e396391387b55f816ebe243a14a68af4cdb241b
Sheikvini <vini5933drf@gmail.com>
2023-11-21 18:25:07 -0300

    pppp

--- react-tcc/src/pages/carrinho/carrinho.js
+++ react-tcc/src/pages/carrinho/carrinho.js
@@ -7,39 +7,41 @@ import storage from "local-storage";
 import { buscarProdutoPorId } from "../../api/produtoAPI";
 import CarrinhoItem from "../../components/carrinhoitem";
 
-export default function Carrinho(){
+export default function Carrinho() {
 
     const [itens, setItens] = useState([]);
-  
 
-    async function carregarCarinho(){
+
+    async function carregarCarinho() {
         let carrinho = storage('carrinho');
-        if (carrinho){
+        if (carrinho) {
 
             let temp = [];
 
             for (let produto of carrinho) {
-               let p = await buscarProdutoPorId(produto.id);   
-              
-               temp.push({
-                produto: p,
-                qtd: produto.qtd
-               })
+                let p = await buscarProdutoPorId(produto.id);
+
+                temp.push({
+                    produto: p,
+                    qtd: produto.qtd
+                })
             }
             console.log(temp);
             setItens(temp);
         }
-        
+
     }
 
     function valortotal() {
+        let total = 0;
         for (let item of itens) {
             
-            itens = item.produto.vl_preco
-         
-        }
+            if (item.produto)
+                total += item.produto.vl_preco * 1
 
-        
+        }
+        return total.toFixed(2)
+
 
     }
 
@@ -47,20 +49,20 @@ export default function Carrinho(){
         carregarCarinho();
     }, [])
 
-return(
-     <header>
-       <Cabecalho/>
-       <section className="principal-carrinho">
-                
+    return (
+        <header>
+            <Cabecalho />
+            <section className="principal-carrinho">
+
                 <h1 className="submenu-carrinho">
                     <Link className='link_C' to={'/carrinho'}>
                         <button className="C">Carrinho</button>
                     </Link>
-                        
+
                     <Link className='link_mp' to={'/rastreio'}>
-                            <button className="MP" >Rastrear</button>
+                        <button className="MP" >Rastrear</button>
                     </Link>
-                    
+
                     <Link className='link_R' to={'/sobrenos'}>
                         <button className="R">Sobre nós</button>
                     </Link>
@@ -69,47 +71,47 @@ return(
                         <button className="TU">Termos de Uso</button>
                     </Link>
                 </h1>
-            
-                <section className="container-carrinho">    
+
+                <section className="container-carrinho">
                     <Link to="/">
-                         <img className='botao-voltar49' src="/assets/img/icon/botao-voltar.png" alt="Voltar" />
+                        <img className='botao-voltar49' src="/assets/img/icon/botao-voltar.png" alt="Voltar" />
                     </Link>
-                    
+
                     <div className="text-MP">
                         <h2 className="Mp">Meu Carrinho</h2>
                     </div>
-                    
-                    
+
+
                 </section>
-                
+
                 <div >
                     <Link className="rastreio-car" to={'https://rastreamento.correios.com.br/app/index.php'}>
-                        <img className="imgrastreio-car" src="/assets/img/cam.png"/>
+                        <img className="imgrastreio-car" src="/assets/img/cam.png" />
                         <p>Rastrear Pedido</p>
                     </Link>
                 </div>
 
-                <div className="carrinho">    
+                <div className="carrinho">
                     <div className="itens">
                         {itens.map((item) =>
                             <CarrinhoItem item={item} />
-                         )}
+                        )}
                     </div>
 
 
-                    
+
 
                     <div className="resumo">
                         <h1>Subtotal</h1>
                         <h3>(3 itens)</h3>
-                        <p>R$ {valortotal} </p>
+                        <p>R$ {valortotal()} </p>
                         <button>Fechar Pedido</button>
                     </div>
                 </div>
-            </section> 
+            </section>
             <footer className="rod-carrinho">
-                <Rodape/>
+                <Rodape />
             </footer>
-     </header>
- )
+        </header>
+    )
 } 
